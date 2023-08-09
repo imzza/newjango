18b51a62997284b37541ba9adc10332be259a3e6
IRFAN ULLAH <irfanullah73@gmail.com>
2023-08-09 12:21:24 +0500

    Added some chanegs

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,15 @@
-from django.urls import include, path
+from django.urls import path
 
-from htv import api_views, func_views
+from . import views
 
 urlpatterns = [
     # fmt: off
-    path("api/", include([
-        path("v1/", include([
-            path("videos", api_views.VideoList.as_view(), name="videos"),
-            path("videos/<int:pk>", api_views.VideoDetail.as_view(), name="video-detail"),
-        ])),
-    ]))
+    path("", views.index, name="index")
+    # path("api/", include([
+    #     path("v1/", include([
+    #         path("videos", api_views.VideoList.as_view(), name="videos"),
+    #         path("videos/<int:pk>", api_views.VideoDetail.as_view(), name="video-detail"),
+    #     ])),
+    # ]))
     # fmt: on
 ]
